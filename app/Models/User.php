6e06532c5217f04ecaf48a22e77bb5467949cf3e
ifigueroator <ifigueroatoro2@gmail.com>
2020-11-19 10:18:29 -0600

    creacion de proyecto laravel 8.15. Se crean proyecto base con usuario password y roles

---
+++ app/Models/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+use App\Models\Role;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+       protected $fillable = [
+        'name', 'email', 'password',
+    ];    protected $hidden = [
+        'password', 'remember_token',
+    ];    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];    public function roles()
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+  
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+ 
+
+
+
+    public function authorizeRoles($roles)
+{
+    abort_unless($this->hasAnyRole($roles), 401);    return true;
+}public function hasAnyRole($roles)
+{
+    if (is_array($roles)) {
+        foreach ($roles as $role) {
+            if ($this->hasRole($role)) {
+                return true;
+            }
+        }
+    } else {
+        if ($this->hasRole($roles)) {
+             return true; 
+        }   
+    }    return false;
+}public function hasRole($role)
+{
+    if ($this->roles()->where('name', $role)->first()) {
+        return true;
+    }    return false;
+}
+
+}
