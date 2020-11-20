e9c7bae2b638bcfca4bbf918d732b1b33ac0fd1e
ifigueroator <ifigueroatoro2@gmail.com>
2020-11-19 19:24:22 -0600

    creacion de vistas y controladores de devoluciones y entregas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+<style type="text/css">
+    
+#titulo{
+
+    color: white;
+    background-color: black;
+    font-size: 120%
+}
+
+</style>
+
+
+
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+
+    <div class="card">
+  <div class="card-header" id="titulo">
+    Ingreso de devoluciones
+  </div>
+  <div class="card-body">
+   
+    <li>Devoluciones x Termino de contrato</li>
+     
+    <li>Devoluciones anticipadas</li>
+    <br>
+    <a href="{{route('devoluciones')}}" class="btn btn-primary">Ir</a>
+  </div>
+</div>
+    </div>
+
+
+
+    <div class="col-sm">
+
+
+
+     <div class="card">
+  <div class="card-header" id="titulo">
+   Entrega de unidades nuevas
+  </div>
+  <div class="card-body">
+    <li>Registro de unidades nuevas</li>
+    
+     <li>Registro de unidades entregadas</li>
+     <br>
+    <a href="{{route('entregas')}}" class="btn btn-primary">Ir</a>
+  </div>
+</div>
+
+
+    </div>
+
+
+
+
+    <div class="col-sm">
+
+
+
+
+    <div class="card">
+  <div class="card-header" id="titulo">
+    Cotizaciones
+  </div>
+  <div class="card-body">
+   <li>Creacion de Cotizaciones</li>
+   <li>Modificacion de Cotizaciones</li>
+   <br>
+   <br>
+
+  <!--  <a href="#" class="btn btn-primary">Go somewhere</a>-->
+  </div>
+</div>
+
+
+
+
+
+
+
+    </div>
+  </div>
+</div>
+
+
+       
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {return view('welcome');});
+
+Route::get('/', function () {return view('../auth/login');});
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('gestion/devoluciones','App\Http\Controllers\viewController@vistadevoluciones')->name('devoluciones');
+
+Route::get('gestion/entregas','App\Http\Controllers\viewController@vistaentregas')->name('entregas');
+
+
+//Route::get('/home', 'HomeController@index')->name('home');
